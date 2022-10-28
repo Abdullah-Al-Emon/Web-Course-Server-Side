a8e7035bb730ce402bf5c94f023b918fd69b3122
abdullah al emon <abdullahemon905@gmail.com>
2022-10-28 22:24:27 +0600

    Server side complete

--- index.js
+++ index.js
@@ -7,15 +7,36 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 
 const course = require('./data/course.json')
+const details = require('./data/details.json')
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) =>
+{
     res.send('web course server is running')
 })
 
-app.get('/new-course', (req, res) => {
+app.get('/new-course', (req, res) =>
+{
     res.send(course)
 })
 
-app.listen(port, () => {
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const course_details = details.filter(c => c.category_id === id)        // console.log(course_details)
+    res.send(course_details);
+
+})
+
+app.get('/details/:id', (req, res) => {
+    const id = req.params.id;
+    const selectDetails = details.find(c => c._id === id)
+    res.send(selectDetails)
+})
+
+app.get('/details', (req, res) => {
+    res.send(details)
+})
+
+app.listen(port, () =>
+{
     console.log(`web course server running on port, ${port}`)
 })
